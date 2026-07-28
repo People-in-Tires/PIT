75667c5c7bab97b92f913ada0b9137bb417aa0dd
Mats de Beer <mde-beer@student.codam.nl>
2026-07-28 19:36:28 +0200

    style(player_interface):

--- ts/game.ts
+++ ts/game.ts
@@ -1,77 +1,83 @@
 console.log("hello");
 
-class inventory implements interactable{
+class inventory implements interactable {
   html_element: HTMLElement;
   item: item | null = null;
 
-  get html() : HTMLElement {
-    return (this.html_element);
+  get html(): HTMLElement {
+    return this.html_element;
   }
-  set html(elem : HTMLElement) {
+  set html(elem: HTMLElement) {
     this.html_element = elem;
   }
-  constructor (clientX : number, clientY : number){
+  constructor(clientX: number, clientY: number) {
     console.log("inv open");
-    this.html_element = createElement("inventory", "<body> empty </body>", clientX, clientY);
+    this.html_element = createElement(
+      "inventory",
+      "<body> empty </body>",
+      clientX,
+      clientY,
+    );
   }
 
   //this refers to the first element up from onmousedown aka html use .bind to bind this to a set value
-  grab_item(ev : MouseEvent) : void{
+  grab_item(ev: MouseEvent): void {
     if (this.item === null) {
-      return ;
+      return;
     }
-    this.item.html.style.left = ev.clientX + "px";
-    this.item.html.style.top = ev.clientY + "px";
+    this.item.html.style.left = `${String(ev.clientX)}px`; // eslint doesnt want you to mix strings and numbers when adding
+    this.item.html.style.top = `${String(ev.clientY)}px`;
     document.body.appendChild(this.item.html);
-    const mouseoverevent = new Event('mousedown');
+    const mouseoverevent = new Event("mousedown");
     this.item.html.dispatchEvent(mouseoverevent);
     this.item = null;
     this.html_element.innerHTML = "";
   }
 
-	interact(elem : draggable | null): void {
-    if (elem === null || elem.html_element === null || this.item)
-      return ;
+  interact(elem: draggable | null): void {
+    if (elem === null || this.item) return;
     this.item = elem as item;
     this.html.innerHTML = elem.html_element.innerHTML;
     this.html.onmousedown = this.grab_item.bind(this);
     document.body.removeChild(elem.html_element);
-	}
+  }
 
-  getRect() : DOMRect{
-    return (this.html_element.getBoundingClientRect());
+  getRect(): DOMRect {
+    return this.html_element.getBoundingClientRect();
   }
 }
 
-class item implements draggable{
+class item implements draggable {
   html_element: HTMLElement;
-  get html() : HTMLElement {
-    return (this.html_element);
+  get html(): HTMLElement {
+    return this.html_element;
   }
-  set html(elem : HTMLElement) {
+  set html(elem: HTMLElement) {
     this.html_element = elem;
   }
-  constructor (elem: HTMLElement){
+  constructor(elem: HTMLElement) {
     this.html_element = elem;
   }
-  getRect() : DOMRect{
-    return (this.html_element.getBoundingClientRect());
+  getRect(): DOMRect {
+    return this.html_element.getBoundingClientRect();
   }
 }
 
 class beer extends item {
-  constructor (clientX : number, clientY : number){
-    super(createElement("draggable", "<img src=./img/beer.png/>", clientX, clientY));
+  constructor(clientX: number, clientY: number) {
+    super(
+      createElement("draggable", "<img src=./img/beer.png/>", clientX, clientY),
+    );
   }
 }
 
 interface draggable extends html {
-  getRect() : DOMRect;
+  getRect(): DOMRect;
 }
 
 interface interactable extends html {
-  getRect() : DOMRect; 
-	interact(elem : draggable | null) : void;
+  getRect(): DOMRect;
+  interact(elem: draggable | null): void;
 }
 
 interface html {
@@ -79,48 +85,48 @@ interface html {
 }
 
 //make constructor for each type of item, in this it makes a new html element that is stored and can be added
-var inv_slots:inventory[] = [];
-var interactables:interactable[] = [];
-var items:item[] = [];
+const inv_slots: inventory[] = [];
+const interactables: interactable[] = [];
+const items: item[] = []; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-var cat : item = new beer(100, 100);
+const cat: item = new beer(100, 100);
 dragElement(cat);
-document.body.appendChild(cat.html)
+document.body.appendChild(cat.html);
 
-for (let i : number = 0; i < 4; i++)
-{
-  let inv_temp = new inventory((i + 1) * 150, 800);
+for (let i = 0; i < 4; i++) {
+  const inv_temp = new inventory((i + 1) * 150, 800);
   inv_slots[i] = inv_temp;
-  document.body.appendChild(inv_temp.html)
+  document.body.appendChild(inv_temp.html);
 }
 //class for
 
-function createElement(classname : string, innerHTML : string , clientX : number, clientY : number) : HTMLElement {
-  var tmp : HTMLDivElement = document.createElement("div");
+function createElement(
+  classname: string,
+  innerHTML: string,
+  clientX: number,
+  clientY: number,
+): HTMLElement {
+  const tmp: HTMLDivElement = document.createElement("div");
   tmp.className = classname;
   tmp.innerHTML = innerHTML;
-  tmp.style.left = clientX + "px";
-  tmp.style.top = clientY + "px";
-  return tmp; 
+  tmp.style.left = `${String(clientX)}px`;
+  tmp.style.top = `${String(clientY)}px`;
+  return tmp;
 }
 
 //add html elements to javascript array interactable, inventory, items
 function dragElement(elem: draggable): void {
-  var pos1: number = 0,
-    pos2: number = 0,
-    pos3: number = 0,
-    pos4: number = 0;
-  if (elem.html_element)
-  {
-    var foo = document.getElementById(elem.html_element.id + "header");
-    if (foo) {
-      // if present, the header is where you move the DIV from:
-      foo.onmousedown = dragMouseDown;
-    } 
-    else {
-      // otherwise, move the DIV from anywhere inside the DIV:
-      elem.html_element.onmousedown = dragMouseDown;
-    }
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  const foo = document.getElementById(elem.html_element.id + "header");
+  if (foo) {
+    // if present, the header is where you move the DIV from:
+    foo.onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elem.html_element.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(ev: MouseEvent): void {
@@ -138,31 +144,24 @@ function dragElement(elem: draggable): void {
     pos3 = ev.clientX;
     pos4 = ev.clientY;
     // set the element's new position:
-    if (elem.html_element)
-    {
-      elem.html_element.style.top = elem.html_element.offsetTop - pos2 + "px";
-      elem.html_element.style.left = elem.html_element.offsetLeft - pos1 + "px";
-    }
+    elem.html_element.style.top = `${String(elem.html_element.offsetTop - pos2)}px`;
+    elem.html_element.style.left = `${String(elem.html_element.offsetLeft - pos1)}px`;
   }
 
   function closeDragElement(ev: MouseEvent): void {
     ev.preventDefault();
     document.onmouseup = null;
     document.onmousemove = null;
-    for (let tmp of interactables)
-    {
-      if (overlap(elem.getRect(), tmp.getRect()))
-        tmp.interact(elem);
+    for (const tmp of interactables) {
+      if (overlap(elem.getRect(), tmp.getRect())) tmp.interact(elem);
     }
-    for (let tmp of inv_slots)
-    {
-      if (overlap(elem.getRect(), tmp.getRect()))
-        tmp.interact(elem);
+    for (const tmp of inv_slots) {
+      if (overlap(elem.getRect(), tmp.getRect())) tmp.interact(elem);
     }
   }
 
   function overlap(elem1: DOMRect, elem2: DOMRect): boolean {
-      return !(
+    return !(
       elem1.right < elem2.left ||
       elem1.left > elem2.right ||
       elem1.bottom < elem2.top ||
