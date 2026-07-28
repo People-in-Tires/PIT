8f87cfc4b6e6b99955bf9534a0dc51fda9d53d69
Laura de Koning <lade-kon@student.codam.nl>
2026-07-28 18:08:17 +0200

    feat: add initial achievement, match and statistics types

---
+++ src/types/match.ts
@@ -0,0 +1,8 @@
+export interface Match
+{
+	id:				number;
+	playerOneId:	number;
+	playerTwoId:	number;
+	winnerId:		number;
+	playedAt:		Date;
+}
---
+++ src/types/statistics.ts
@@ -0,0 +1,14 @@
+export interface Statistics
+{
+	userId:	number;
+
+	matchesPlayed:	number;
+	wins:			number;
+	losses:			number;
+
+	totalPitStops:		number;
+	perfectPitStops:	number;
+	fastestPitStopTime:	number;
+
+	totalCrashes:	number;
+}
