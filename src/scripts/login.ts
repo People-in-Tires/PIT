35b51f296b900b4143d7bce8a13241bd755f00de
Laura de Koning <lade-kon@student.codam.nl>
2026-08-05 16:25:57 +0200

    style: applied Prettier formatting to scripts files

--- src/scripts/login.ts
+++ src/scripts/login.ts
@@ -2,36 +2,33 @@ const form = document.getElementById("login-form") as HTMLFormElement;
 const loginInput = document.getElementById("login") as HTMLInputElement;
 const passwordInput = document.getElementById("password") as HTMLInputElement;
 
-form.addEventListener("submit", async function(event)
-{
-	event.preventDefault();
-	
-	const login = loginInput.value;
-	const password = passwordInput.value;
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
 
-	if (login == "" || password == "")
-	{
-		console.log("Please fill in all fields.");
-		return;
-	}
-	console.log(login);
-	console.log(password);
-	console.log("Sending login request");
+  const login = loginInput.value;
+  const password = passwordInput.value;
 
-	await loginUser(login, password);
+  if (login == "" || password == "") {
+    console.log("Please fill in all fields.");
+    return;
+  }
+  console.log(login);
+  console.log(password);
+  console.log("Sending login request");
+
+  await loginUser(login, password);
 });
 
-async function loginUser(login: string, password: string)
-{
-	const response = await fetch("api/login", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json"
-		},
-		body: JSON.stringify({
-			login: login,
-			password: password
-		})
-	});
-	console.log(response.status);
+async function loginUser(login: string, password: string) {
+  const response = await fetch("api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      login: login,
+      password: password,
+    }),
+  });
+  console.log(response.status);
 }
