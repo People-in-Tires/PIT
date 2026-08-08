e62e9eb567d9e16d8fe1f5269f336edf28867893
Milan Nijsen <mnijsen@codam.nl>
2026-08-08 16:54:17 +0200

    feature(minigame) first react implemintation

---
+++ pit/app/objects/inventory.tsx
@@ -0,0 +1,81 @@
+import styles from "../Index.module.css";
+import React, { useState } from "react";
+import { createContext } from "vm";
+import { useContext } from "react";
+import { InventoryContext } from "../page";
+//use context containing  astate
+
+export default function Inventory({
+  slots = 10,
+  size = 0.75,
+}: {
+  slots?: number;
+  size?: number;
+}) {
+  const rows: React.JSX.Element[] = [];
+  const inventory = useContext(InventoryContext);
+  if (inventory === undefined || inventory.inventory === undefined) return;
+  for (let i = 0; i < slots; i++) {
+    rows.push(
+      <InventorySlots
+        xPer={(100 / slots) * (i + (1 - size) / 2)}
+        size={(100 / slots) * size}
+        content={React.createElement("div", inventory.inventory[i])}
+        key={i}
+      />,
+    );
+  }
+  return <div id="inventory">{rows}</div>;
+}
+
+function InventorySlots({
+  xPer,
+  size,
+  content,
+}: {
+  xPer: number;
+  size: number;
+  content: React.JSX.Element | null;
+}) {
+  return (
+    <div
+      className={styles.inventory}
+      style={{
+        left: `${xPer}%`,
+        bottom: "5%",
+        width: `${size}%`,
+        aspectRatio: 1,
+      }}
+    >
+    </div>
+  );
+}
+
+export const InventoryFunctions = (function () {
+  //get ref to slots for collision and add function for collision detection
+  // const [inventory, setInventory] = useState<(HTMLElement | null)[]>([]);
+  let inventory: (HTMLElement | null)[] = [];
+  function init(slots: number) {
+    let tmp: (HTMLElement | null)[] = [];
+    for (let i = 0; i < slots; i++)
+      //have slots be a macro
+      tmp.push(null);
+    inventory = tmp;
+  }
+  function addContent(content: HTMLElement, index: number) {
+    const newContent = inventory.map((c, i) => {
+      if (i === index) {
+        return content;
+      } else {
+        return c;
+      }
+    });
+    console.log(inventory);
+    inventory = newContent;
+  }
+  return {
+    addContent,
+    init,
+    inventory,
+  };
+})();
--- pit/app/page.tsx
+++ pit/app/page.tsx
@@ -1,65 +1,56 @@
+"use client";
+import Beer from "./objects/beer";
+import GameButton from "./objects/GameButton";
+import React, {
+  createContext,
+  createElement,
+  ReactElement,
+  useState,
+} from "react";
 import Image from "next/image";
+import Inventory from "@/app/objects/inventory";
+//usecontext for inventory and interactions
+//InventoryContext at top layer
+export interface InvContext {
+  inventory?: (React.JSX.Element | null)[];
+  setInventory?: any;
+}
+export const InventoryContext = createContext<InvContext | undefined>(
+  undefined,
+);
 
 export default function Home() {
+  const [windows, setWindows] = useState<React.JSX.Element[]>([]);
+  const [inventory, setInventory] = useState<(React.JSX.Element | null)[]>([]);
+  if (inventory.length == 0)
+    {
+    let tmp: (React.JSX.Element | null)[] = [];
+    for (let i = 0; i < 10; i++)
+      //have slots be a macro
+    tmp.push(null);
+    setInventory(tmp);
+  }
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+      <InventoryContext value={{ inventory, setInventory }}>
+        <Inventory />
+        <GameButton src="/minigames/grill" img="/grill.png" windows={windows} setWindows={setWindows}/>
+        <button
+          onClick={() => {
+            setWindows([...windows, <Beer />]);
+          }}
+          id="BeerButton"
+        >
+          <Image
+            draggable="false"
+            src={"/beer.png"}
+            width={150}
+            height={150}
+            alt="beercrate"
+          />
+        </button>
+        {windows}
+      </InventoryContext>
     </div>
   );
 }
