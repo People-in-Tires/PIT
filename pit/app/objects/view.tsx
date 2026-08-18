d359f1044856d7b90b95e7ae3cd0589d67f712a8
Simon <svan-hoo@student.codam>
2026-08-18 17:32:08 +0200

    feat: View (backgrounds, buttons), Laptop (iframe)

---
+++ pit/app/objects/view.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import styles from "../css/Index.module.css";
+import Home, { VIEW } from "../page";
+import { ViewContext } from "../page";
+import React, { useContext, useState } from "react";
+import Laptop from "./laptop";
+
+const backgrounds: string[] = [
+  "/background-brick-1.jpg",
+  "/background-brick-2.jpg",
+  "/background-brick-2.jpg",
+];
+
+export default function View() {
+  const context = useContext(ViewContext);
+  if (context === undefined) return;
+  const { view, setView } = context;
+
+  if (view < 0 || view >= VIEW.end) setView(VIEW.garage);
+
+  return (
+    <div>
+      <Image
+        src={backgrounds[view]}
+        width={2560}
+        height={1440}
+        alt="background"
+        style={{
+          position: "absolute",
+          opacity: 1,
+          width: "100%",
+          height: "100%",
+          aspectRatio: "2",
+          objectFit: "cover",
+        }}
+      />
+      {view === VIEW.garage && <Car />}
+      {view === VIEW.laptop && <Laptop />}
+      <ViewButtons />
+    </div>
+  );
+}
+
+function Car() {
+  return (
+    <Image
+      src="/car2.png"
+      alt="car"
+      width={2560}
+      height={1440}
+      style={{
+        position: "absolute",
+        bottom: 0,
+        left: "30%",
+        opacity: 0.5,
+        width: "50%",
+        height: "50%",
+        objectFit: "contain",
+      }}
+    />
+  );
+}
+
+function ViewButtons() {
+  const context = useContext(ViewContext);
+  if (context === undefined) return;
+  const { view, setView } = context;
+
+  const left = (
+    <button id={`${styles.LeftButton}`} onClick={() => setView(view - 1)}>
+      <Image src={"/angle-left.svg"} fill={true} alt="arrowLeft" />
+    </button>
+  );
+
+  const right = (
+    <button id={`${styles.RightButton}`} onClick={() => setView(view + 1)}>
+      <Image src={"/angle-right.svg"} fill={true} alt="arrowRight" />
+    </button>
+  );
+
+  return (
+    <div>
+      {view > 0 && left}
+      {view < VIEW.end - 1 && right}
+    </div>
+  );
+}
--- pit/app/page.tsx
+++ pit/app/page.tsx
@@ -3,6 +3,7 @@ import Beer from "./objects/beer";
 import GameButton from "./objects/GameButton";
 import React, { useState, createContext } from "react";
 import Image from "next/image";
+import View from "./objects/view";
 import Inventory from "@/app/objects/inventory";
 import { useRef } from "react";
 import Car from "./objects/car";
@@ -12,17 +13,32 @@ export interface StateContext {
   state: React.JSX.Element[];
   setState: React.Dispatch<React.SetStateAction<React.JSX.Element[]>>;
 }
-
 export const GameWindowContext = createContext<StateContext | undefined>(
   undefined,
 );
 
+export enum VIEW {
+  garage = 0,
+  bench,
+  laptop,
+  end,
+}
+export interface viewContext {
+  view: VIEW;
+  setView: React.Dispatch<React.SetStateAction<VIEW>>;
+}
+export const ViewContext = createContext<viewContext | undefined>(undefined);
+
 export default function Home() {
+  const [view, setView] = useState<VIEW>(VIEW.laptop);
   const [windows, setWindows] = useState<React.JSX.Element[]>([]);
   const car = useRef(new Car()); //replace with get info from sim when the car rolls in
 
   return (
     <GameWindowContext value={{ state: windows, setState: setWindows }}>
+      <ViewContext value={{ view, setView }}>
+        <View />
+      </ViewContext>
       <Inventory />
       <Wrench />
       <button
