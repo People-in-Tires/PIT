43f6ab15cc6210ded15ae663b22dfa71f97307e7
Mats de Beer <mde-beer@student.codam.nl>
2026-08-21 17:44:52 +0200

    feat(docker): docker compose
    builds all dependencies (including local rust dep), then runs the server
    in a container

--- pit/components/wasmExample.tsx
+++ pit/components/wasmExample.tsx
@@ -3,23 +3,23 @@ import { SimulationContext } from "@/context/simulation";
 import { Point } from "@/simulation/wasm_simulation";
 
 export const WASMExample = () => {
-	const ctx = useContext(SimulationContext);
-	if (!ctx) {
-		return <>...</>;
-	}
-	const points: Point[] = [];
-	for (let i = 0; i < 8; i++) {
-		points[i] = new Point(1, 2);
-	}
-	for (const p of points.slice(0, 3)) points.push(p.clone());
-	return (
-		<>
-			points:
-			<ol>
-				{points.map((p) => {
-					return p.string();
-				})}
-			</ol>
-		</>
-	);
+  const ctx = useContext(SimulationContext);
+  if (!ctx) {
+    return <>...</>;
+  }
+  const points: Point[] = [];
+  for (let i = 0; i < 8; i++) {
+    points[i] = new Point(1, 2);
+  }
+  for (const p of points.slice(0, 3)) points.push(p.clone());
+  return (
+    <>
+      points:
+      <ol>
+        {points.map((p) => {
+          return p.string();
+        })}
+      </ol>
+    </>
+  );
 };
