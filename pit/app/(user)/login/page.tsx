995558f86393cc8d08dae3fa7dd437d855cad0b9
Laura de Koning <lade-kon@student.codam.nl>
2026-08-21 19:20:09 +0200

    feat: improve login page (styling and layout) and authentication logic

--- pit/app/(user)/login/page.tsx
+++ pit/app/(user)/login/page.tsx
@@ -1,42 +1,39 @@
-'use client'
+"use client";
 
 import { signin } from "@/app/actions/auth";
 import { useActionState } from "react";
+import "./login.css";
 
 export default function Login() {
-	const [state, action, pending] = useActionState(signin, undefined)
+  const [state, action, pending] = useActionState(signin, undefined);
 
-	return (
-		<div className="flex items-center flex-col">
-			<img className="size-100" id="logo" src="/PIT.png" alt="Logo" />
-		<h1 className="text-5xl/25">Login</h1>
-		<form action={action} className="text-2xl flex items-center flex-col">
-			<div>
-				<label htmlFor="login">Login: </label>
-				<input id="login" name="login" placeholder="username or email" />
-			</div>
-			{state?.errors?.login && <p>{state.errors.login}</p>}
-			<div>
-				<label htmlFor="password">Password: </label>
-				<input id="password" name="password" type="password" placeholder="********" />
-			</div>
-			{state?.errors?.password && (
-			  <div>
-				<p>Password must:</p>
-				<ul>
-					{state.errors.password.map((error) => (
-						<li key={error}>- {error}</li>
-					))}
-				</ul>
-			</div>
-			)}
-			<div id="forgot-password" className="text-sm/10">
-				<button type="button">Forgot Password</button>
-			</div>
-			<div className="text-2xl/20">
-				<button disabled={pending} type="submit">Enter the PIT</button>
-			</div>
-		</form>
-		</div>
-	);
+  return (
+    <div className="flex items-center flex-col">
+      <img className="size-100" id="logo" src="/PIT.png" alt="Logo" />
+      <h1 className="text-5xl/25">Login</h1>
+      {state?.success && <p className="success">{state.success}</p>}
+      <form action={action} className="text-2xl flex items-center flex-col">
+        <div>
+          <label htmlFor="login">Login: </label>
+          <input id="login" name="login" placeholder="username or email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="********"
+          />
+        </div>
+        {state?.message && <p className="error">{state.message}</p>}
+        <button className="login" disabled={pending} type="submit">
+          Enter the PIT
+        </button>
+        <div id="forgot-password" className="forgot-password">
+          <button type="button">Forgot Password</button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- pit/app/actions/auth.ts
+++ pit/app/actions/auth.ts
@@ -3,7 +3,12 @@
 import { z } from "zod";
 import bcrypt from "bcryptjs";
 import { prisma } from "../lib/prisma";
-import { NewUserFormState, NewUserFormSchema, LoginFormState, LoginFormSchema } from "@/app/lib/definitions";
+import {
+  NewUserFormState,
+  NewUserFormSchema,
+  LoginFormState,
+  LoginFormSchema,
+} from "@/app/lib/definitions";
 
 z.config(z.locales.en()); //zod errors always in english
 
@@ -14,7 +19,6 @@ export async function signup(state: NewUserFormState, formData: FormData) {
     lastname: formData.get("lastname"),
     birthday: formData.get("birthday"),
     country: formData.get("country"),
-    city: formData.get("city"),
     email: formData.get("email"),
     password: formData.get("password"),
     password2: formData.get("password2"),
@@ -23,10 +27,19 @@ export async function signup(state: NewUserFormState, formData: FormData) {
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
+      values: {
+        username: formData.get("username") as string,
+        firstname: formData.get("firstname") as string,
+        lastname: formData.get("lastname") as string,
+        birthday: formData.get("birthday") as string,
+        country: formData.get("country") as string,
+        city: formData.get("city") as string,
+        email: formData.get("email") as string,
+      },
     };
   }
 
-  const { username, firstname, lastname, birthday, country, email, password, password2 } =
+  const { username, firstname, lastname, birthday, country, email, password } =
     validatedFields.data;
 
   const passwordHash = await bcrypt.hash(password, 10);
@@ -57,17 +70,18 @@ export async function signup(state: NewUserFormState, formData: FormData) {
 
     console.error("Error creating user:", error);
     return {
-      message: "Something went wrong while creating your account. Please try again.",
+      message:
+        "Something went wrong while creating your account. Please try again.",
     };
   }
 
-  return { message: "Account created successfully!" };
+  return { success: "Account created successfully!" };
 }
 
 export async function signin(state: LoginFormState, formData: FormData) {
   const validatedFields = LoginFormSchema.safeParse({
     login: formData.get("login"),
-    password: formData.get("password")
+    password: formData.get("password"),
   });
 
   if (!validatedFields.success) {
@@ -79,23 +93,20 @@ export async function signin(state: LoginFormState, formData: FormData) {
   const { login, password } = validatedFields.data;
 
   const user = await prisma.user.findFirst({
-  where: {
-    OR: [
-      { email: login.toLowerCase() },
-      { username: login },
-    ],
-  },
+    where: {
+      OR: [{ email: login.toLowerCase() }, { username: login }],
+    },
   });
 
   if (!user) {
-  return { message: "User not found" }
+    return { message: "User not found" };
   }
 
   const passwordCorrect = await bcrypt.compare(password, user.passwordHash);
 
   if (!passwordCorrect) {
-     return { message: "Incorrect password!" }
+    return { message: "Incorrect password!" };
   }
 
-  return { message: "Entering the PIT"}
+  return { success: "Entering the PIT" };
 }
