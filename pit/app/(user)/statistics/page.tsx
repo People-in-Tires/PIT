5ec7784b658f5c78022191bcb3b966bfc4ef4e28
Laura de Koning <lade-kon@student.codam.nl>
2026-08-25 16:08:07 +0200

    style: update statistics page .css

--- pit/app/(user)/statistics/page.tsx
+++ pit/app/(user)/statistics/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useActionState } from "react";
-import "./profile.css";
+import "./statistics.css";
 
 export default function Statistics() {
   // const [state, action, pending] = useActionState(signin, undefined)
